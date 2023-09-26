d3ff94929adb402bfd37e112298271c91abd94b3
Andreas <andreas_poulsen@hotmail.com>
2023-09-26 21:31:16 +0200

    2022/day1 part 2

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -18,6 +18,10 @@ func main() {
     elfIndex := 0
     sumCal := 0
     highestCal := 0
+    topThreeElves := make(map[int]int)
+    topThreeElves[0] = 0;
+    topThreeElves[1] = 0;
+    topThreeElves[2] = 0;
     for i := 0; i < len(lines); i++ {
         if len(lines[i]) == 0 {
             sumCal = 0
@@ -33,8 +37,24 @@ func main() {
         if sumCal > highestCal {
             highestCal = sumCal
             elfIndex = i
+
+        }
+
+        if(sumCal > topThreeElves[0]) {
+            topThreeElves[2] = topThreeElves[1]
+            topThreeElves[1] = topThreeElves[0]
+            topThreeElves[0] = sumCal
+        }else if(sumCal > topThreeElves[1]) {
+            topThreeElves[2] = topThreeElves[1]
+            topThreeElves[1] = sumCal
+        }else if(sumCal > topThreeElves[2]) {
+            topThreeElves[2] = sumCal
         }
     }
     fmt.Println("Highest Calorie: ", highestCal)
     fmt.Println("Elf Index: ", elfIndex)
+
+    fmt.Println("Top Three Elves: ", topThreeElves)
+    topThreeSum := topThreeElves[0] + topThreeElves[1] + topThreeElves[2]
+    fmt.Println("Top Three Sum: ", topThreeSum)
 }
